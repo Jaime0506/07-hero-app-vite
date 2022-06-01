26814b50f0072005245aa4e11dd2d0155791c2fa
Jaime Mejia <jaimemejia1003@gmail.com>
2022-05-31 22:00:21 -0500

    Ya casi

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -5,7 +5,7 @@ import DashBoardRoutes from "./DashBoardRoutes"
 
 const AppRouter = () => {
     return (
-        <BrowserRouter>
+        <BrowserRouter basename="https://jaime0506.github.io/07-hero-app-vite/">
             <Routes>
                 <Route path="/login" element={<LoginScreen />} />
                 <Route path="/*" element={<DashBoardRoutes />} />
