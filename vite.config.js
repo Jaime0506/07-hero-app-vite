98549266e0a7821d6fccd69d0bef088fc9ad387e
Jaime Mejia <jaimemejia1003@gmail.com>
2022-05-31 22:33:01 -0500

    vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/07-hero-app-vite/',
+  base: '/07-hero-app-vite/',
   preview: {
     port: 4000
   },
